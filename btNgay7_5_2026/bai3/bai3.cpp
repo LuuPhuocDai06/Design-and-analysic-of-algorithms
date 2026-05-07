26b3596f49b088af37378acf15b5ac5c4e0d18e8
LuuPhuocDai06 <dailuuphuoc@gmail.com>
2026-05-07 22:34:03 +0700

    add bai3

---
+++ btNgay7_5_2026/bai3/bai3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void h() {
+    int n;
+    if (!(cin >> n)) return;
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    sort(b.begin(), b.end());
+
+    int m = 0; 
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if ((b[i] + b[j]) % 2 == 0) {
+                int d = 0;
+                for (int k = 0; k < n; k++) {
+                    if (b[k] >= b[i] && b[k] <= b[j]) {
+                        d++;
+                    }
+                }
+                if (d > m) m = d;
+            }
+        }
+    }
+    if (n == 0) cout << 0 << endl;
+    else cout << n - m << endl;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int q;
+    if (!(cin >> q)) return 0;
+    while (q--) {
+        h();
+    }
+    return 0;
+}
