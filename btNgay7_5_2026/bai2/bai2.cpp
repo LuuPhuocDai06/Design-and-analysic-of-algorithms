a9b2f2ebd0af7cc1f8bbf121ed9733a7960340dc
LuuPhuocDai06 <dailuuphuoc@gmail.com>
2026-05-07 22:12:57 +0700

    add bai2

---
+++ btNgay7_5_2026/bai2/bai2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+void g() {
+    vector<int> m(4);
+    for (int i = 0; i < 4; i++) {
+        if (!(cin >> m[i])) return;
+    }
+
+    sort(m.begin(), m.end());
+
+    if (m[0] == m[3]) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int w;
+    if (!(cin >> w)) return 0;
+    while (w--) {
+        g();
+    }
+    return 0;
+}
